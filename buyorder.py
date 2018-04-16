ef4f2187fa12c06071c1d5d094ba62dd853d2418
majy2016 <372840680@qq.com>
2018-04-17 00:47:04 +0800

    增加接口查询

--- buyorder.py
+++ buyorder.py
@@ -12,6 +12,8 @@ https://buff.163.com/api/market/goods/sell_order?game=pubg&goods_id=
 
 """
 import requests,json,ssl
+from bs4 import BeautifulSoup
+
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -73,6 +75,7 @@ def balanceChenck():
     print(r.status_code)
     print(r.text)
 
+
 # balanceChenck()
 
 
@@ -109,7 +112,28 @@ def createBuyOrder(goods_id,price,num,pay_method):
 
 #查询求购信息
 # https://buff.163.com/market/buy_order/wait_supply?game=pubg
+def getBuyList():
+    url = "https://buff.163.com/market/buy_order/wait_supply?game=pubg"
+    headers = {
+        "Host":"buff.163.com",
+        "Origin":"https://buff.163.com",
+        "Referer":"https://buff.163.com/market/buy_order/wait_supply?game=pubg",
+        "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+        "Cookie":"_ntes_nnid=40da4c75ce28ee3fd276a23789989ebb,1521015755470; _ntes_nuid=40da4c75ce28ee3fd276a23789989ebb; Province=0571; City=0571; __gads=ID=007456be95b9b848:T=1522813945:S=ALNI_MZrL3JIF0TJANFO5gRTe31usiVOgw; UM_distinctid=1628ec8d0ac70a-04b7f0def9e281-6a11157a-1fa400-1628ec8d0ad7f6; vjuids=d95d59992.1628ec8d662.0.540aa0c179edd; vinfo_n_f_l_n3=db93eb31cf6ddd34.1.1.1522813949576.1522813980223.1523340533497; vjlast=1522813950.1523589129.11; game=pubg; NTES_YD_SESS=js.DZrgDjBdaT_UZA98sx_z30NTUu1Gi0zvL43Ir12Z9rOhUrIJ5Lfh3CBYRfO.xGwjIgkwlKFxXuJ2ZOMkLi9HuFf7VIunsv5GYv04.v6wLsGR8RZb8wleEw.lOpMFMlUX0tVl51pV3DYPAyVKy8cfQrWu0RiYUYktIjmj_YdtXOoL4riCZXD69qxi4xT.MllMbkwncwbeVGf5Wyvcbs1hAXIewlfpjl8oC4PrkJJDAQ; S_INFO=1523843557|0|3&80##|15158009950; P_INFO=15158009950|1523843557|1|netease_buff|00&99|null&null&null#zhj&330100#10#0|&0||15158009950; _ga=GA1.2.72461816.1520413701; _gid=GA1.2.1866669209.1523842226; locale=zh; session=1-8fGn0QiF088A8uymAJ7FtmFy3n55uIQj4iAyb1w2oVx02045356209; csrf_token=dc90c423792d0d509f63f5227f8578bf7283f5c3",
+        "X-CSRFToken":"ImRjOTBjNDIzNzkyZDBkNTA5ZjYzZjUyMjdmODU3OGJmNzI4M2Y1YzMi.DbWb7Q.DX1hDwcW0RGjKN5-d5VJyzlR5IE"
+                }
+    x = requests.session()
+    r = x.get(url=url,headers=headers)
+    print(r.status_code)
+    # print(r.text)
+    bs = BeautifulSoup(r.text,"lxml")
+    # print(bs)
+    list = bs.findAll("a",{"class":"i_Btn cancel-buy-order i_Btn_hollow"})
+    for i in list:
+        print(i["data-orderid"])
 
+
+# getBuyList()
 
 # 求购取消
 # https://buff.163.com/api/market/buy_order/cancel
@@ -123,6 +147,7 @@ def cancelBuyOrder(order_id):
     print(data)
     headers = {
         "Host":"buff.163.com",
+        'Content-Type': 'application/json',
         "Origin":"https://buff.163.com",
         "Referer":"https://buff.163.com/market/buy_order/wait_supply?game=pubg",
         "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
@@ -130,8 +155,66 @@ def cancelBuyOrder(order_id):
         "X-CSRFToken":"ImRjOTBjNDIzNzkyZDBkNTA5ZjYzZjUyMjdmODU3OGJmNzI4M2Y1YzMi.DbWb7Q.DX1hDwcW0RGjKN5-d5VJyzlR5IE"
                 }
     x = requests.session()
-    r = x.post(url=url,data=data,headers=headers)
+    r = x.post(url=url,data=json.dumps(data),headers=headers)
     print(r.status_code)
     print(r.text)
 
-cancelBuyOrder("180416T1094483989")
+# cancelBuyOrder("180416T1094493203")
+
+
+#查询售卖列表
+# https://buff.163.com/market/sell_order/on_sale?game=pubg
+def getSellList():
+    url = "https://buff.163.com/market/sell_order/on_sale?game=pubg"
+    headers = {
+        "Host":"buff.163.com",
+        "Origin":"https://buff.163.com",
+        "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+        "Cookie":"_ntes_nnid=40da4c75ce28ee3fd276a23789989ebb,1521015755470; _ntes_nuid=40da4c75ce28ee3fd276a23789989ebb; Province=0571; City=0571; __gads=ID=007456be95b9b848:T=1522813945:S=ALNI_MZrL3JIF0TJANFO5gRTe31usiVOgw; UM_distinctid=1628ec8d0ac70a-04b7f0def9e281-6a11157a-1fa400-1628ec8d0ad7f6; vjuids=d95d59992.1628ec8d662.0.540aa0c179edd; vinfo_n_f_l_n3=db93eb31cf6ddd34.1.1.1522813949576.1522813980223.1523340533497; vjlast=1522813950.1523589129.11; game=pubg; NTES_YD_SESS=js.DZrgDjBdaT_UZA98sx_z30NTUu1Gi0zvL43Ir12Z9rOhUrIJ5Lfh3CBYRfO.xGwjIgkwlKFxXuJ2ZOMkLi9HuFf7VIunsv5GYv04.v6wLsGR8RZb8wleEw.lOpMFMlUX0tVl51pV3DYPAyVKy8cfQrWu0RiYUYktIjmj_YdtXOoL4riCZXD69qxi4xT.MllMbkwncwbeVGf5Wyvcbs1hAXIewlfpjl8oC4PrkJJDAQ; S_INFO=1523843557|0|3&80##|15158009950; P_INFO=15158009950|1523843557|1|netease_buff|00&99|null&null&null#zhj&330100#10#0|&0||15158009950; _ga=GA1.2.72461816.1520413701; _gid=GA1.2.1866669209.1523842226; locale=zh; session=1-8fGn0QiF088A8uymAJ7FtmFy3n55uIQj4iAyb1w2oVx02045356209; csrf_token=dc90c423792d0d509f63f5227f8578bf7283f5c3",
+        "X-CSRFToken":"ImRjOTBjNDIzNzkyZDBkNTA5ZjYzZjUyMjdmODU3OGJmNzI4M2Y1YzMi.DbWb7Q.DX1hDwcW0RGjKN5-d5VJyzlR5IE"
+                }
+    x = requests.session()
+    r = x.get(url=url,headers=headers)
+    print(r.status_code)
+    # print(r.text)
+    bs = BeautifulSoup(r.text,"lxml")
+    # print(bs)
+    list = bs.findAll("li",{"class":"salable"})
+    for i in list:
+        print(i["data-orderid"])
+
+# getSellList()
+
+#改价
+#https://buff.163.com/api/market/sell_order/change
+def changePrice(goods_id,sell_order_id,price):
+    url = "https://buff.163.com/api/market/sell_order/change"
+
+    headers = {
+        "Host":"buff.163.com",
+        "Origin":"https://buff.163.com",
+        'Content-Type': 'application/json',
+        "Referer":"https://buff.163.com/market/sell_order/on_sale?game=pubg",
+        "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+        "Cookie":"_ntes_nnid=40da4c75ce28ee3fd276a23789989ebb,1521015755470; _ntes_nuid=40da4c75ce28ee3fd276a23789989ebb; Province=0571; City=0571; __gads=ID=007456be95b9b848:T=1522813945:S=ALNI_MZrL3JIF0TJANFO5gRTe31usiVOgw; UM_distinctid=1628ec8d0ac70a-04b7f0def9e281-6a11157a-1fa400-1628ec8d0ad7f6; vjuids=d95d59992.1628ec8d662.0.540aa0c179edd; vinfo_n_f_l_n3=db93eb31cf6ddd34.1.1.1522813949576.1522813980223.1523340533497; vjlast=1522813950.1523589129.11; game=pubg; NTES_YD_SESS=js.DZrgDjBdaT_UZA98sx_z30NTUu1Gi0zvL43Ir12Z9rOhUrIJ5Lfh3CBYRfO.xGwjIgkwlKFxXuJ2ZOMkLi9HuFf7VIunsv5GYv04.v6wLsGR8RZb8wleEw.lOpMFMlUX0tVl51pV3DYPAyVKy8cfQrWu0RiYUYktIjmj_YdtXOoL4riCZXD69qxi4xT.MllMbkwncwbeVGf5Wyvcbs1hAXIewlfpjl8oC4PrkJJDAQ; S_INFO=1523843557|0|3&80##|15158009950; P_INFO=15158009950|1523843557|1|netease_buff|00&99|null&null&null#zhj&330100#10#0|&0||15158009950; _ga=GA1.2.72461816.1520413701; _gid=GA1.2.1866669209.1523842226; locale=zh; session=1-8fGn0QiF088A8uymAJ7FtmFy3n55uIQj4iAyb1w2oVx02045356209; csrf_token=dc90c423792d0d509f63f5227f8578bf7283f5c3",
+        "X-CSRFToken":"ImRjOTBjNDIzNzkyZDBkNTA5ZjYzZjUyMjdmODU3OGJmNzI4M2Y1YzMi.DbWb7Q.DX1hDwcW0RGjKN5-d5VJyzlR5IE"
+                }
+
+    data = {
+        "game": "pubg",
+        "sell_orders": [{"sell_order_id": sell_order_id, "price":price, "goods_id": goods_id}]
+    }
+    print(data)
+    x = requests.session()
+    r = x.post(url=url,data=json.dumps(data),headers=headers)
+    print(r.status_code)
+    print(r.text)
+
+changePrice(33232,"180417T1095187850",0.6)
+
+
+#todo
+#查询背包列表
+
+#寄售上架
+
